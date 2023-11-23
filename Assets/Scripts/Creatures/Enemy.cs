fcb545bf1a608890bade68524b07bf6936929c30
Диэр <danil.aprelskiy@mail.ru>
2023-11-23 14:01:13 +0300

    feat: spawner range, enemy moving
    Spawner создает новых врагов в рандомном месте на промежутке;
    Спаунер при создании врага заставляет того двигаться в нужном направлении;
    Из Enemy убрано патрулирование, вместо этого можно задать направление движения.

--- Assets/Scripts/Creatures/Enemy.cs
+++ Assets/Scripts/Creatures/Enemy.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 public class Enemy : Creature, IInteractable
 {
     private float _movementSpeed;
-    private float _patrolingTime;
 
     protected override void Start()
     {
         base.Start();
         SetRandomSpeed();
-        SetPatrolingTime();
-        StartCoroutine(Patroling());
     }
 
     protected override void Update()
@@ -30,21 +27,11 @@ public class Enemy : Creature, IInteractable
         _movementSpeed = Random.Range(0.1f, 0.49f);
     }
 
-    private void SetPatrolingTime()
+    public void Move(bool isRight)
     {
-        _patrolingTime = Random.Range(0.3f, 1f);
-    }
-
-    private IEnumerator Patroling()
-    {
+        SetRandomSpeed();
+        _movementSpeed *= isRight ? 1 : -1;
         InputVector = new Vector2(_movementSpeed, 0);
-
-        yield return new WaitForSeconds(_patrolingTime);
-
-        InputVector = new Vector2(-_movementSpeed, 0);
-
-        yield return new WaitForSeconds(_patrolingTime);
-        StartCoroutine(Patroling());
     }
 
     public void Interact(Creature creature)
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,12 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _objectToSpawn;
-    [SerializeField] private GameObject _spawnPoint;
+    [SerializeField] private Enemy _enemyPrefab;
+    [SerializeField] private bool _isRightSpawner;
+
+    [Header("Spawn Range")]
+    [SerializeField] private float _xMinPoint;
+    [SerializeField] private float _xMaxPoint;
+    [SerializeField] private float _yMinPoint;
+    [SerializeField] private float _yMaxPoint;
+
+    private Vector3 _spawnPoint;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnTimer());
+    }
+
+    private IEnumerator SpawnTimer()
+    {
+        Spawn();
+
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(SpawnTimer());
+    }
+
+    private void CalculateSpawnPosition()
+    {
+        var xPosition = Random.Range(_xMinPoint, _xMaxPoint);
+        var yPosition = Random.Range(_yMinPoint, _yMaxPoint);
+
+        _spawnPoint = new Vector3(xPosition, yPosition, 0f);
+    }
 
     public void Spawn()
     {
-        Instantiate(_objectToSpawn, _spawnPoint.transform.position, Quaternion.identity);
+        CalculateSpawnPosition();
+        var enemy = Instantiate(_enemyPrefab, _spawnPoint, Quaternion.identity);
+        enemy.Move(!_isRightSpawner);
     }
 }
