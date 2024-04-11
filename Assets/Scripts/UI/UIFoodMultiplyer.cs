1937cd1d0d512dd0ab7569885032663863a78b20
Диэр <danil.aprelskiy@mail.ru>
2024-04-11 14:27:30 +0300

    fix: disable food multiply button if food = 0

--- Assets/Scripts/UI/UIFoodMultiplyer.cs
+++ Assets/Scripts/UI/UIFoodMultiplyer.cs
@@ -5,6 +5,12 @@ public class UIFoodMultiplyer : MonoBehaviour
 {
     [SerializeField] private LevelFoodSaver _foodSaver;
 
+    private void OnEnable()
+    {
+        if(_foodSaver.Player.FoodCount == 0)
+            Destroy(gameObject);
+    }
+
     public void MultiplyFood()
     {
         _foodSaver.SaveFoodCount();
