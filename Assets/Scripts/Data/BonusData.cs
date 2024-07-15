b15892e8cf2d92ea256d5ac475c80fb196a0c970
Диэр <danil.aprelskiy@mail.ru>
2024-07-16 00:03:42 +0300

    feat: add more fields for bonus data (name, id)

--- Assets/Scripts/Data/BonusData.cs
+++ Assets/Scripts/Data/BonusData.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New BonusData", order = 51)]
 public class BonusData : ScriptableObject
 {
+    [SerializeField] private int _id;
+    [SerializeField] private string _bonusName;
     [SerializeField] private Sprite _bonusIcon;
     [SerializeField] private float _timeBeforeDestroying = 5f;
 
@@ -12,20 +14,27 @@ public class BonusData : ScriptableObject
     [SerializeField] private float _costForLevel;
     [SerializeField] private float _bonusTimeForLevel;
 
-    public float TimeBeforeDestroying => _timeBeforeDestroying;
+    private int _level = 1;
+
+    public int ID => _id;
     public Sprite BonusIcon => _bonusIcon;
+    public string BonusName => _bonusName;
+    public float TimeBeforeDestroying => _timeBeforeDestroying;
 
     public float BaseBonusTime => _baseBonusTime;
     public float BaseCostTime => _baseCost;
 
     public float CostForLevel => _costForLevel;
     public float BonusTimeForLevel => _bonusTimeForLevel;
+    
+    public int Level => _level;
 
-    private int _level = 1;
-
-    public void AddLevel()
+    public void SetLevel(int level)
     {
-        _level++;
+        if (level <= 0)
+            level = 1;
+
+        _level = level;
     }
 
     public float TotalBonusTime()
