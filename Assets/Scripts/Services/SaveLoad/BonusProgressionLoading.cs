b96e42708feb60bdfaf45be092d2f10718087cfd
Диэр <danil.aprelskiy@mail.ru>
2024-07-17 21:58:58 +0300

    fix: bonus data caching in bonus progression loading

--- Assets/Scripts/Services/SaveLoad/BonusProgressionLoading.cs
+++ Assets/Scripts/Services/SaveLoad/BonusProgressionLoading.cs
@@ -5,21 +5,31 @@ public class BonusProgressionLoading : MonoBehaviour
     [SerializeField] private string _pathToBonusData = "Data/BonusData";
     [SerializeField] private BonusProgression _bonusProgressionData;
 
+    private BonusData[] _bonusData;
+
     private void Start()
     {
         SetBonusProgression();
     }
 
+    public BonusData[] GetCurrentBonusData()
+    {
+        if (_bonusData == null)
+            _bonusData = Resources.LoadAll<BonusData>(_pathToBonusData);
+
+        return _bonusData;
+    }
+
     private void SetBonusProgression()
     {
-        var allBonusData = Resources.LoadAll<BonusData>(_pathToBonusData);
+        var allBonusData = GetCurrentBonusData();
         var allBonusDataProgression = _bonusProgressionData.GetData();
 
         foreach (var bonusData in allBonusData)
         {
             foreach (var bonusDataProgression in allBonusDataProgression.Datas)
             {
-                if(bonusData.BonusInfo.ID == bonusDataProgression.ID)
+                if (bonusData.BonusInfo.ID == bonusDataProgression.ID)
                 {
                     bonusData.BonusInfo.SetLevel(bonusDataProgression.Level);
                     Debug.Log($"Bonus with id {bonusData.BonusInfo.ID} now has level {bonusData.BonusInfo.Level}");
