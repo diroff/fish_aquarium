1c3eac4cbeeeaaa80c722cb4447a5ad7b856cff9
Диэр <danil.aprelskiy@mail.ru>
2024-08-23 01:23:28 +0300

    feat: add prefab for ui one time bonus

--- Assets/Scripts/UI/UIOnetimeBonus.cs
+++ Assets/Scripts/UI/UIOnetimeBonus.cs
@@ -4,10 +4,24 @@ using UnityEngine.UI;
 
 public class UIOnetimeBonus : MonoBehaviour
 {
+    [SerializeField] private Image _bonusSprite;
+    [SerializeField] private Image _blockerSprite;
+
     [SerializeField] private TextMeshProUGUI _bonusCountText;
     [SerializeField] private Button _bonusButton;
 
     [SerializeField] private OnetimeBonus _bonus;
+    [SerializeField] private OnetimeBonusUsing _bonusUsing;
+
+    private void Awake()
+    {
+        _bonusSprite.sprite = _bonus.BonusData.BonusIcon;
+    }
+
+    private void Start()
+    {
+        _bonusButton.onClick.AddListener(() => _bonusUsing.UseBonus(_bonus));
+    }
 
     private void OnEnable()
     {
@@ -24,6 +38,8 @@ public class UIOnetimeBonus : MonoBehaviour
         _bonusCountText.text = count.ToString();
 
         bool buttonEnabled = count > 0;
+
         _bonusButton.enabled = buttonEnabled;
+        _blockerSprite.enabled = !buttonEnabled;
     }
 }
